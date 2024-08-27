9408fb813ca678b8793592fe7a45a1790cbbd661
Jasen Tenney <jasen_tenney@hotmail.com>
2024-08-27 09:26:14 -0500

    Added page to list all containers with ability to start or stop based on current state

--- nextui/next.config.mjs
+++ nextui/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: [
+        {
+          loader: 'nextjs-node-loader',
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
