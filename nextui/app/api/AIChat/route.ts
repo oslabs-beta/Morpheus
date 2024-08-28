6b6e3e25626a963432b85aef6eadd8085dd2a328
dariusteng <dariusyteng@gmail.com>
2024-08-28 13:37:47 -0400

    Merged dev into branch, removed prom K query

--- nextui/app/api/AIChat/route.ts
+++ nextui/app/api/AIChat/route.ts
@@ -62,12 +62,12 @@ export async function POST(request: Request) {
 Only if the question is relevant to Kubernetes, proceed to answer the question using the Kubernetes metrics data I will provide to offer insights and recommendations. (If it is relevant, the fact that it is relevant should not be addressed.)
 Here is the Kubernetes data: ${JSON.stringify(kubernetesData, null, 2)}.
 Never use placeholders if you do not know specific numbers or information. Be concise yet informative.
-Aim to organize the response by metrics and include specific pods/services/deployments and their metrics/numbers, if relevant. Do not use bold or heading formatting. Limit your response to under 1500 tokens.`;
+Aim to organize the response by metrics and include specific pods/services/deployments and their metrics/numbers, if relevant. Use line breaks, numbering, bullets, and bolding for organization. Limit your response to under 1500 tokens.`;
 
     //gpt-4 seems to have much better understanding of what information is considered relevant, as well as better consistency in response style and length, so it didn't require these specific instructions.
     //However, 3.5-turbo was used because it was much cheaper at less than $0.005 per 1000 tokens. while 4 was over $0.03 per 1000 tokens.
     //For container (not k8) query: running Morpheus containers alone, the prometheus data sent is about 4 to 6 thousand tokens. The complexity of the data seems to also increase this, because just the prometheus data, with no true prompt and limited response, would cost 3 to 6 cents per request in gpt-4.
-    //Used 'opinion' in prompt to get more judgement rather than only objective facts. Mentioned formatting because bold results in '**' and headings result in '###'.
+    //Used 'opinion' in prompt to get more judgement rather than only objective facts. In some models, disallow certain formatting. Bold can result in '**' and headings in '###'.
     //Organization by metric was used for more clarity.Used 'containers/metrics/numbers' to prevent vague or general sounding responses.
     //Several questions can be representative of how strict their interpretation of the rules are. From least to most answered given the same rules for interpretation and response: 'how is the weather', 'where is california', 'how are my containers', 'how is my cluster'.
     //For the most strict rules, use terms like 'act strictly as...' and 'without exception'. However, this combo made 3.5 believe questions like 'how is my cluster' not relevant.
